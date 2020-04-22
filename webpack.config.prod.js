3fac41dd3aaaf96ffbcf050c70738ee1197fda67
Valentin <valentin.martin33@gmail.com>
2020-04-22 15:12:37 +0200

    refactor: update webpack, babel transpilation

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,62 +1,63 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 try {
-  require('os').networkInterfaces()
+  require("os").networkInterfaces();
 } catch (e) {
-  require('os').networkInterfaces = () => ({})
+  require("os").networkInterfaces = () => ({});
 }
 
 module.exports = {
-  entry: [
-    './src/app/index.js',
-  ],
+  entry: ["./src/app/index.js"],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    libraryTarget: 'umd'
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    libraryTarget: "umd",
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /.js?$/,
-        loaders: ['babel-loader'],
+        loaders: ["babel-loader"],
         exclude: /node_modules/,
         include: path.resolve(__dirname),
       },
       {
         test: /\.css$/,
-        loaders: ["style-loader", "css-loader"]
+        loaders: ["style-loader", "css-loader"],
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader?limit=10000&mimetype=application/octet-stream"
-      }, {
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream",
+      },
+      {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader"
-      }, {
+        loader: "file-loader",
+      },
+      {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-      }, {
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+      },
+      {
         test: /\.(jpg|jpeg|gif|png)$/,
         exclude: /node_modules/,
-        loader: 'url-loader?limit=65000&name=images/[name].[ext]'
-      }
-    ]
+        loader: "url-loader?limit=65000&name=images/[name].[ext]",
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': `"production"`
-      }
+      "process.env": {
+        NODE_ENV: `"production"`,
+      },
     }),
     new HtmlWebpackPlugin({
       inject: true,
-      template: './public/index.html'
+      template: "./public/index.html",
     }),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
 };
