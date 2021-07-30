67e36678aec1c20312dfdfb0b2ea2ee39db5abea
Valentin <valentin.martin@dolby.com>
2021-07-30 14:01:43 +0200

    CCP-2118 SDK 3.2.0 support and improvements

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import thunkMidleware from 'redux-thunk'
-import promiseMiddleware from 'redux-promise'
-import { combineReducers, createStore, applyMiddleware } from 'redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import thunkMidleware from "redux-thunk";
+import promiseMiddleware from "redux-promise";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 
-import { ConferenceRoom, reducer as voxeetReducer } from './VoxeetReactComponents'
+import {
+  ConferenceRoom,
+  reducers as voxeetReducer,
+} from "./VoxeetReactComponents";
 
 const reducers = combineReducers({
-  voxeet: voxeetReducer
-})
+  voxeet: voxeetReducer,
+});
 
-const configureStore = () => createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunkMidleware, promiseMiddleware)
-)
+const configureStore = () =>
+  createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunkMidleware, promiseMiddleware)
+  );
 
 ReactDOM.render(
   <Provider store={configureStore()}>
@@ -24,5 +29,5 @@ ReactDOM.render(
       />
     </div>
   </Provider>,
-  document.getElementById('app')
-)
+  document.getElementById("app")
+);
